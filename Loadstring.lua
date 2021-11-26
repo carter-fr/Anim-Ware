ca3c8211a2ad1a619b83facd870c206659f883d6
carter <81034605+carter-fr@users.noreply.github.com>
2021-11-25 18:38:19 -0700

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -9,5 +9,5 @@ _G.Theme = "Serpent"
 | $$  | $$| $$  | $$| $$| $$ | $$ | $$| $$  | $$|  $$$$$$/| $$$$$$$/
 |__/  |__/|__/  |__/|__/|__/ |__/ |__/|__/  |__/ \______/ |_______/ 
 ]]
--- AnimHub by carterfr#9709, skid all you want.
+-- AnimHub by carterfr#9709, skid all you want. Themes include: LightTheme, DarkTheme, GrapeTheme, BloodTheme, Ocean, Midnight, Sentinel, Synapse and Serpent
 loadstring(game:HttpGet("https://raw.githubusercontent.com/carter-fr/Anim-Ware/main/Anim-Ware.lua"))()
