d39892509eca2dee1d57aef1429ac82e22f9b7dd
carter <81034605+carter-fr@users.noreply.github.com>
2023-06-30 15:17:37 -0600

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -9,5 +9,5 @@ _G.Theme = "Ocean"
 | $$  | $$| $$  | $$| $$| $$ | $$ | $$        | $$/   \  $$|  $$$$$$$| $$      |  $$$$$$$
 |__/  |__/|__/  |__/|__/|__/ |__/ |__/        |__/     \__/ \_______/|__/       \_______/
 ]]
--- Anim-Ware by carterfr#9709, skid all you want. Themes include: LightTheme, DarkTheme, GrapeTheme, BloodTheme, Ocean, Midnight, Sentinel, Synapse and Serpent
+-- Anim-Ware by .carterfr on Discord, skid all you want. Themes include: LightTheme, DarkTheme, GrapeTheme, BloodTheme, Ocean, Midnight, Sentinel, Synapse and Serpent
 loadstring(game:HttpGet("https://raw.githubusercontent.com/carter-fr/Anim-Ware/main/Anim-Ware.lua"))()
